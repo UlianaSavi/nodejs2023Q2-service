493e1c89fd6542b30f83735b7f8007e271f02bf2
Uliana Savitskaua <uljasha.sav@gmail.com>
2023-08-19 20:39:15 +0600

    feat: add loggin for req & res data in users

--- src/Logger/logger.service.ts
+++ src/Logger/logger.service.ts
@@ -3,6 +3,7 @@ import { promises as fs } from 'fs';
 import { ConsoleLogger, Injectable, Scope } from '@nestjs/common';
 import { LogLevel } from '@nestjs/common/services';
 import { createWriteStream } from 'fs';
+import { ILog } from './models/reqLog.model';
 
 @Injectable({ scope: Scope.TRANSIENT })
 export class CustomLoggerService extends ConsoleLogger {
@@ -14,31 +15,83 @@ export class CustomLoggerService extends ConsoleLogger {
     console.log(message);
   }
 
-  async check(fileToRead: string) {
-    try {
-      await fs.access(fileToRead, fs.constants.F_OK);
-      return true;
-    } catch (err) {
-      return false;
-    }
-  }
+  async writeLogToFile(data: ILog, filePath: string, context?: string) {
+    const file = createWriteStream(join(process.cwd(), filePath), {
+      flags: 'a',
+    });
 
-  async writeLogToFile(message: string, context: string) {
-    const file = createWriteStream(
-      join(process.cwd(), 'src/Logger/logs/errors.log'),
-      { flags: 'a' },
-    );
-    try {
-      const chunk = `Message: ${message} ----- Time: ${new Date().getTime()} ----- Context: ${context}\n`;
-      file.write(chunk);
-      return chunk;
-    } catch (err) {
-      console.log('Error in logger: ', err?.message);
+    if (data.type === 'error') {
+      try {
+        const chunk = `Message: ${
+          data?.message
+        } ----- Time: ${new Date().getTime()} ----- Context: ${
+          context || 'unknown'
+        }\n`;
+
+        file.write(chunk);
+        return chunk;
+      } catch (err) {
+        console.log('Error in logger: ', err?.message);
+      }
+    }
+    if (data.type === 'requestLog') {
+      try {
+        const chunk = `url: ${data?.url} ----- query: ${
+          data?.query
+        } ----- body: ${JSON.stringify(data?.body, null, 4)}\n`;
+
+        file.write(chunk);
+        return chunk;
+      } catch (err) {
+        console.log('Error in logger: ', err?.message);
+      }
+    }
+    if (data.type === 'responseLog') {
+      try {
+        const chunk = `statusCode: ${
+          data?.statusCode
+        } Time: ${new Date().getTime()}\n`;
+
+        file.write(chunk);
+        return chunk;
+      } catch (err) {
+        console.log('Error in logger: ', err?.message);
+      }
     }
   }
 
   async error(message: string, context: string) {
-    const log = await this.writeLogToFile(message, context);
-    super.error(`Logger wtite new error log ----> \n${log}`);
+    const filePath = 'src/Logger/logs/errors.log';
+    const errorToLog: ILog = {
+      type: 'error',
+      message,
+    };
+    const log = await this.writeLogToFile(errorToLog, filePath, context);
+
+    super.error(`Logger wtite new ERROR log ----> \n${log}`);
+  }
+
+  async requestLog(url: string, query: string, body: string) {
+    const filePath = 'src/Logger/logs/request.log';
+    const dataToLog: ILog = {
+      type: 'requestLog',
+      url,
+      query,
+      body,
+    };
+    const log = await this.writeLogToFile(dataToLog, filePath);
+
+    super.log(`Logger wtite new REQUEST log ----> \n${log}`);
+  }
+
+  async responseLog(statusCode: number) {
+    const filePath = 'src/Logger/logs/response.log';
+    const dataToLog: ILog = {
+      type: 'responseLog',
+      statusCode,
+    };
+    const log = await this.writeLogToFile(dataToLog, filePath);
+
+    super.log(`Logger wtite new RESPONSE log ----> \n${log}`);
   }
 }
--- src/Users/user.controller.ts
+++ src/Users/user.controller.ts
@@ -1,14 +1,27 @@
 import { Controller, Get, Post, Put, Delete, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { UserService } from './user.service';
+import { CustomLoggerService } from 'src/Logger/logger.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private logger: CustomLoggerService,
+  ) {}
 
   @Get()
-  async getAll(@Res() res: Response) {
+  async getAll(@Req() req: Request, @Res() res: Response) {
+    this.logger.requestLog(
+      req?.url || '',
+      JSON.stringify(req?.query, null, 4),
+      req?.body || '',
+    );
+
     const userRes = await this.userService.getAll();
+
+    this.logger.responseLog(userRes.statusCode);
+
     res.status(userRes.statusCode);
     res.send(userRes.data);
   }
@@ -18,7 +31,15 @@ export class UserController {
     const { params } = req;
     const id = params.id;
 
+    this.logger.requestLog(
+      req?.url || '',
+      JSON.stringify(req?.query, null, 4),
+      req?.body || '',
+    );
+
     const userRes = await this.userService.getById(id);
+
+    this.logger.responseLog(userRes.statusCode);
 
     res.status(userRes.statusCode);
     res.send(userRes.data);
@@ -28,7 +49,15 @@ export class UserController {
   async create(@Req() req: Request, @Res() res: Response) {
     const { body } = req;
 
+    this.logger.requestLog(
+      req?.url || '',
+      JSON.stringify(req?.query, null, 4),
+      req?.body || '',
+    );
+
     const userRes = await this.userService.createUser(body);
+
+    this.logger.responseLog(userRes.statusCode);
 
     res.status(userRes.statusCode);
     res.send(userRes.data);
@@ -39,7 +68,16 @@ export class UserController {
     const { body, params } = req;
     const id = params.id;
 
+    this.logger.requestLog(
+      req?.url || '',
+      JSON.stringify(req?.query, null, 4),
+      body || '',
+    );
+
     const userRes = await this.userService.updateUserPassword(id, body);
+
+    this.logger.responseLog(userRes.statusCode);
+
     res.status(userRes.statusCode);
     res.send(userRes.data);
   }
@@ -49,7 +87,16 @@ export class UserController {
     const { params } = req;
     const id = params.id;
 
+    this.logger.requestLog(
+      req?.url || '',
+      JSON.stringify(req?.query, null, 4),
+      req?.body || '',
+    );
+
     const userRes = await this.userService.deleteUser(id);
+
+    this.logger.responseLog(userRes.statusCode);
+
     res.status(userRes.statusCode);
     res.send(userRes.data);
   }
